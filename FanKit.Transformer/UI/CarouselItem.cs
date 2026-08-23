c766441e15c597c7110dfd9e788c65dd79b59165
ysdy44 <ysdy44@outlook.com>
2026-08-23 14:28:35 +0800

    File Combine: CarouselItem1 + CarouselItem2 -> CarouselItem

--- FanKit.Transformer/UI/CarouselItem.cs
+++ FanKit.Transformer/UI/CarouselItem.cs
@@ -3,7 +3,17 @@ using System.Numerics;
 
 namespace FanKit.Transformer.UI
 {
-    public readonly struct CarouselItem2
+    public readonly partial struct CarouselItem1
+    {
+        public readonly CarouselState State;
+        public readonly float Amount;
+
+        public readonly Quadrilateral TextureOutline;
+
+        public readonly Matrix4x4 TextureTransformMatrix;
+    }
+
+    public readonly partial struct CarouselItem2
     {
         public readonly CarouselState State;
         public readonly float Amount;
@@ -14,7 +24,41 @@ namespace FanKit.Transformer.UI
         public readonly Quadrilateral TextureOutline;
 
         public readonly Matrix4x4 TextureTransformMatrix;
+    }
 
+    partial struct CarouselItem1
+    {
+        // -0.5 ~ +0.5
+        internal CarouselItem1(Carousel carousel, SizeMatrix sourceNormalize, float centerX, float centerY, float amount)
+        {
+            if (amount < -0.5f)
+            {
+                State = CarouselState.DockLeft;
+                Amount = -0.5f;
+
+                TextureOutline = carousel.GetDockLeftTextureOutline(centerX, centerY);
+            }
+            else if (amount > 0.5f)
+            {
+                State = CarouselState.DockRight;
+                Amount = 0.5f;
+
+                TextureOutline = carousel.GetDockRightTextureOutline(centerX, centerY);
+            }
+            else
+            {
+                State = CarouselState.Float;
+                Amount = amount;
+
+                TextureOutline = carousel.GetFloatTextureOutline(centerX, centerY, this.Amount);
+            }
+
+            TextureTransformMatrix = sourceNormalize.ToPerspMatrix(TextureOutline);
+        }
+    }
+
+    partial struct CarouselItem2
+    {
         internal CarouselItem2(Carousel carousel, SizeMatrix sourceNormalize, int index, float centerX, float centerY, float offsetX, float itemMargin, float itemSpacing)
         {
             RawX = offsetX + index * itemSpacing;
